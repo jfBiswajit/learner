799ab431fd2d4ffe93ad95d71f6203b8334e1b35
Biswajit Biswas <jfbiswajit@gmail.com>
2021-02-19 20:25:44 +0600

    Init: section two

---
+++ play.js
@@ -0,0 +1,10 @@
+let name = 'Biswajit';
+let age = 23;
+let hasHobby = true;
+
+function getUserInfo(name, age, hasHobby) {
+  return 'Hey this is ' + name + ' and he is ' + age + ' years old.';
+}
+
+
+console.log(getUserInfo(name, age, hasHobby));
